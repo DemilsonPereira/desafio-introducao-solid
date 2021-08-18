cdd91b0aeed5de5eb75eb61d3c9c32b8793817e5
demilsonpereira <demilson.d2102@gmail.com>
2021-08-17 21:05:56 -0400

    Done

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,23 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const idUser = this.usersRepository.findById(user_id);
+
+    if (!idUser) {
+      throw new Error("Usuário não existe");
+    }
+
+    if (!idUser.admin) {
+      throw new Error("Usuário não é admin");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
+
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,10 +3,20 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const userUpdate = this.turnUserAdminUseCase.execute({
+        user_id
+      });
+
+      return response.status(200).json(userUpdate);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
